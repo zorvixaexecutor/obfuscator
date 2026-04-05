1c07d48a025dae50202e086b30f17ab1033eb78c
Zorvixa <rivalsreporthub@gmail.com>
2026-04-05 12:36:18 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,23 @@
 local slf = {}
 
-function slf:pri()
-  print("suck me")
+function slf:obfuscate(str, key)
+    local t = {}
+    for i = 1, #str do
+        local c = string.byte(str, i)
+        local k = string.byte(key, 1 + ((i-1) % #key))
+        table.insert(t, string.char(bit32.bxor(c, k)))
+    end
+    return table.concat(t)
+end
+
+function slf:deobfuscate(gibberish, key)
+    local t = {}
+    for i = 1, #gibberish do
+        local c = string.byte(gibberish, i)
+        local k = string.byte(key, 1 + ((i-1) % #key))
+        table.insert(t, string.char(bit32.bxor(c, k)))
+    end
+    return table.concat(t)
 end
 
 return slf
