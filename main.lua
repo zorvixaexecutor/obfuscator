9d95cbbf995f0a93935447289c40c722c4ec87cc
Zorvixa <rivalsreporthub@gmail.com>
2026-04-05 14:30:42 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,54 +1,57 @@
 local slf = {}
 
 local function toHex(str)
-    return (str:gsub(".", function(c)
-        return string.format("%02X", string.byte(c))
-    end))
+	return (str:gsub(".", function(c)
+		return string.format("%02X", string.byte(c))
+	end))
 end
 
 local function fromHex(hex)
-    return (hex:gsub("..", function(cc)
-        return string.char(tonumber(cc, 16))
-    end))
+	return (hex:gsub("..", function(cc)
+		return string.char(tonumber(cc, 16))
+	end))
 end
+
 function slf:makekey(str)
-    local key = tostring(math.random(1000000000000,9999999999999))
-    local t = {}
-    for i = 1, #str do
-        local c = string.byte(str, i)
-        local k = string.byte(key, 1 + ((i-1) % #key))
-        t[i] = string.char(bit32.bxor(c, k))
-    end
-    return toHex(table.concat(t)), key
+	local key = tostring(math.random(100000000,999999999))
+	local t = {}
+	for i = 1, #str do
+		local c = string.byte(str, i)
+		local k = string.byte(key, 1 + ((i-1) % #key))
+		t[i] = string.char(bit32.bxor(c, k))
+	end
+	return toHex(table.concat(t)), key
 end
-function slf:key(key, key2)
-    local t = {}
-    for i = 1, #key do
-        local c = string.byte(key, i)
-        local k = string.byte(key2, 1 + ((i-1) % #key2))
-        t[i] = string.char(bit32.bxor(c, k))
-    end
-    return toHex(table.concat(t))
+
+function slf:key(str,key)
+	local t = {}
+	for i = 1, #str do
+		local c = string.byte(str, i)
+		local k = string.byte(key, 1 + ((i-1) % #key))
+		t[i] = string.char(bit32.bxor(c, k))
+	end
+	return toHex(table.concat(t))
 end
+
 function slf:obfuscate(str, key)
-    local t = {}
-    for i = 1, #str do
-        local c = string.byte(str, i)
-        local k = string.byte(key, 1 + ((i-1) % #key))
-        t[i] = string.char(bit32.bxor(c, k))
-    end
-    return toHex(table.concat(t))
+	local t = {}
+	for i = 1, #str do
+		local c = string.byte(str, i)
+		local k = string.byte(key, 1 + ((i-1) % #key))
+		t[i] = string.char(bit32.bxor(c, k))
+	end
+	return toHex(table.concat(t))
 end
 
 function slf:deobfuscate(hex, key)
-    local gibberish = fromHex(hex)
-    local t = {}
-    for i = 1, #gibberish do
-        local c = string.byte(gibberish, i)
-        local k = string.byte(key, 1 + ((i-1) % #key))
-        t[i] = string.char(bit32.bxor(c, k))
-    end
-    loadstring(table.concat(t))()
+	local gibberish = fromHex(hex)
+	local t = {}
+	for i = 1, #gibberish do
+		local c = string.byte(gibberish, i)
+		local k = string.byte(key, 1 + ((i-1) % #key))
+		t[i] = string.char(bit32.bxor(c, k))
+	end
+	loadstring(table.concat(t))()
 end
 
 return slf
